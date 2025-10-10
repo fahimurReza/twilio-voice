90bd9402c444788fbd5c10dbe52e8d79a8967794
Auvy <fahimur@gmail.com>
2025-10-11 01:54:13 +0600

    style update

--- client/src/twilioDialler/HistoryCard.jsx
+++ client/src/twilioDialler/HistoryCard.jsx
@@ -30,7 +30,7 @@ function HistoryCard({ call, index }) {
 
   return (
     <div>
-      <div className="mx-3 py-1 px-3 rounded-md flex items-center hover:bg-blue-50 group">
+      <div className="mx-3 py-1 px-3 rounded-md flex items-center hover:bg-blue-50 group border-b border-blue-200">
         <div className="rounded-md bg-blue-100 group-hover:bg-white group-hover:border group-hover:border-blue-300 flex justify-center items-center w-10 h-10">
           <BsPersonFill size={20} />
         </div>
@@ -39,22 +39,22 @@ function HistoryCard({ call, index }) {
           <div className="text-[13px] text-gray-600">{call.business}</div>
         </div>
         <div
-          className="ml-auto rounded-md justify-center items-center w-10 h-10 cursor-pointer hidden group-hover:flex bg-white border border-red-200"
+          className="ml-auto rounded-md justify-center items-center w-9 h-9 cursor-pointer hidden group-hover:flex bg-white border border-red-200"
           onClick={handleDelete}
         >
           <MdDelete size={24} color="#ef4444" />
         </div>
         <div
-          className="ml-2 rounded-lg justify-center items-center w-10 h-10 cursor-pointer hidden group-hover:flex bg-white border border-blue-300"
+          className="ml-2 rounded-lg justify-center items-center w-9 h-9 cursor-pointer hidden group-hover:flex bg-white border border-blue-300"
           onClick={handleCall}
         >
           <FaPhoneFlip size={18} color="#2563eb" />
         </div>
         <div className="ml-auto h-12 flex flex-col justify-center group-hover:hidden">
+          <div className="text-base text-right text-gray-600">{call.time}</div>
           <div className="text-[13px] text-gray-600 text-right">
             {call.date}
           </div>
-          <div className="text-base text-right text-gray-600">{call.time}</div>
         </div>
       </div>
     </div>
